433dcdeb1970dccf18be52a7ccf69bb30701122a
Nagi.akc <samyakchiche04@gmail.com>
2025-11-19 00:20:58 +0100

    Media Queries: Made the navBar responsive to 1024px screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 export default function Navbar() {
     return (
-        <div className=" flex items-center justify-between px-64 py-2">
+        <div className=" flex items-center justify-between px-64 lg:px-48 py-2">
             <Header />
             <NavLinks />
         </div>
